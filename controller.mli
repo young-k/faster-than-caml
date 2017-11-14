9edfcff4b92f46f6ffd955130813c2548e397a52
Kevin Chan <kevinchan159@gmail.com>
2017-11-14 18:42:46 -0500

    removed command.ml and added command type to controller.mli

--- controller.mli
+++ controller.mli
@@ -3,7 +3,18 @@ open Ship
 open Galaxy
 open Event
 open Store
-open Command
+
+type command =
+  | Attack of (int * string)  (* Choose a weapon in slot to attack a room *)
+  | Choice of bool            (* Choosing an option (y/n) for events *)
+  | CloseMap                  (* Gets rid of the display of the map *)
+  | Equip of (string * int)   (* Equip a weapon to a certain slot *)
+  | Go of int                 (* Go to another star *)
+  | Look                      (* Look at reachable stars *)
+  | Power of string           (* Get the power level of a system *)
+  | Purchase of string        (* Purchase an item (weapon/augmentation) from a store *)
+  | ShowMap                   (* Displays the map *)
+  | None
 
 type screen_type =
   | HomeScreen
