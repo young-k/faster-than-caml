abe949c236e869c16b087eac0e29a4fe862ae9c7
Megan Le <meganle7@gmail.com>
2017-12-03 19:51:00 -0500

    Add current star to sidebar

--- main.ml
+++ main.ml
@@ -29,6 +29,7 @@ let rec loop t c =
   (* sidebar fixture *)
   let score = new label ("Score: " ^ string_of_int c.score) in
   let jumps = new label ("Jumps: " ^ string_of_int c.jumps) in
+  let star = new label ("Current star: " ^ string_of_int c.star_id) in
   let ship = c.ship in
   let resources = Ship.get_resources ship in 
   let hull = Ship.get_hull ship in
@@ -45,6 +46,7 @@ let rec loop t c =
   let sidebar = new vbox in 
   sidebar#add ~expand:false score;
   sidebar#add ~expand:false jumps;
+  sidebar#add ~expand:false star;
   sidebar#add ~expand:false new hline;
   sidebar#add ~expand:false scrap;
   sidebar#add ~expand:false fuel;
