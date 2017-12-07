29a162f69a64804f6a9bcd443501b86b9ef2cbcc
Megan Le <meganle7@gmail.com>
2017-12-07 12:54:19 -0500

    Controller specification for ShowCurrentScreeN

--- controller.mli
+++ controller.mli
@@ -18,7 +18,7 @@ type command =
   | ShowShipScreen            (* Screen for details on ship & ship upgrades *)
   | ShowHomeScreen            (* Show home screen *)
   | ShowInstructions          (* Show instructions screen *)
-  | ShowCurrentScreen
+  | ShowCurrentScreen         (* Shows the current screen of a controller *)
 
 (* screen_type contains information about what to display on UI *)
 type screen_type =
