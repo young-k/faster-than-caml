1ff854580c8a22ce57f7b28e1e6c3b8cb5e605bf
Young Kim <young-k@protonmail.com>
2017-12-06 22:44:06 -0500

    federation -> Federation

--- controller.ml
+++ controller.ml
@@ -75,11 +75,11 @@ let is_purchase com =
 
 let parse_command c com =
   if c.jumps >= 7 then {c with screen_type=
-    GameOver "You have been caught by the federation. Rest in pieces."}
+    GameOver "You have been caught by the Federation. Rest in pieces."}
   else if c.ship.hull <=0 && not (is_purchase com) then {c with screen_type=
     GameOver "Your ship has been damaged beyond repair and has fallen apart."}
   else if get_fuel c.ship <=0 && not (is_purchase com) then {c with screen_type=
-    GameOver "You have run out of fuel, with no way to escape the federation."}
+    GameOver "You have run out of fuel, with no way to escape the Federation."}
   else
   match com with
   | ShowHomeScreen -> {c with screen_type=HomeScreen}
