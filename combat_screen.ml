a0214959bdf05df79a5b3843799e78da20984dee
Young Kim <young-k@protonmail.com>
2017-12-07 14:39:58 -0500

    added enemy

--- combat_screen.ml
+++ combat_screen.ml
@@ -210,11 +210,13 @@ let get_components combat ship (m, h, s) (waiter, wakener) =
            combat := se;
            winner := th;
            let player = (!combat).player in
+           let enemy = (!combat).enemy in
            let resources = Ship.get_resources player in 
            m#set_text ("Missiles: " ^ string_of_int resources.missiles);
            h#set_text ("Hull: " ^ string_of_int (Ship.get_hull player));
            s#set_text ("Shield Level: " ^ 
                        string_of_int (player.shield.layers));
+
            text_label#set_text (!text)) in
 
   ((map, mainbox), (selected_weapon, event), combat);
